88b788ce12cdf4c0b6834e185430a7b65ab391cc
Jelena Tesic <jtesic@txstate.edu>
2020-12-17 18:55:30 -0600

    graphB ver 2.0

--- graphB/postprocess/wrapper_postprocess_general.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sbatch --dependency=afterany:$5 postprocess/postprocess_general.sh $1 $2 $3 $4 $5
--- graphB/scripts/sbatch_generic_plots.sh
+++
@@ -1,8 +0,0 @@
-declare -a plot_options=(22)
-echo "|++++++++++| Putting in a bunch of sbatch jobs:"
-for plot in "${plot_options[@]}"
-do
-    sbatch sbatch_singular_plot.sh $plot &
-    echo "|+| Submitted batch job: plot $plot"
-done
-echo "|++++++++++| sbatch jobs sent in."
